e7b5630b0803fcd3bce0058efa48aaf9327dac17
PassiveLemon <72527881+PassiveLemon@users.noreply.github.com>
2022-08-07 18:37:28 -0400

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 VERSION=2022.07.58.8
-
-if [ -e "contents/ModPacks/*/Mods/enabled.json" ]; then
+if [ -e contents/ModPacks/*/Mods/enabled.json ]; then
   ./startserver.sh
 else
   if [ ! -d "Libraries/" ]; then
