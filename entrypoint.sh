23448264da5641bce7c9e27a84789c0bac5b5098
PassiveLemon <72527881+PassiveLemon@users.noreply.github.com>
2022-08-09 23:59:04 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 VERSION=2022.07.58.8
 
-if [ -e contents/ModPacks/*/Mods/enabled.json ]; then
+if [ -e ModPacks/*/Mods/enabled.json ]; then
   ./startserver.sh
 else
   if [ ! -d "Libraries/" ]; then
@@ -14,12 +14,12 @@ else
     curl -LO https://github.com/PassiveLemon/tmodloader1.4-docker/releases/download/${VERSION}/tmodloader1.4-docker-master.zip
     unzip -o tmodloader1.4-docker-master.zip
     rm -r tmodloader1.4-docker-master.zip
-    mkdir contents/ModPacks/
-    mkdir contents/Worlds/
+    mkdir ModPacks/
+    mkdir Worlds/
     chmod +x startserver.sh
     chmod +x start-tModLoaderServer.sh
-      echo "Server setup complete"
+    echo "Server setup complete"
   fi
-  echo "No modpack detected. Add your modpack to contents/ModPacks/ and restart."
+  echo "No modpack detected. Add your modpack to ModPacks/ and restart."
   exit
 fi
