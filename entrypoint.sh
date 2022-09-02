b4f00d16631f53edd8ad793ed1cd8d96536e62ad
PassiveLemon <72527881+PassiveLemon@users.noreply.github.com>
2022-09-02 12:21:06 -0400

    Add files via upload

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-VERSION=2022.07.58.8
+VERSION=2022.08.54.3
 
-if [ -e contents/ModPacks/*/Mods/enabled.json ]; then
+if [ -e ModPacks/*/Mods/enabled.json ]; then
   ./startserver.sh
 else
   if [ ! -d "Libraries/" ]; then
@@ -14,12 +14,12 @@ else
     curl -LO https://github.com/PassiveLemon/tmodloader1.4-docker/releases/download/${VERSION}/tmodloader1.4-docker-master.zip
     unzip -o tmodloader1.4-docker-master.zip
     rm -r tmodloader1.4-docker-master.zip
-    mkdir contents/ModPacks/
-    mkdir contents/Worlds/
+    mkdir ModPacks/
+    mkdir Worlds/
     chmod +x startserver.sh
     chmod +x start-tModLoaderServer.sh
-      echo "Server setup complete"
+    echo "Server setup complete"
   fi
-  echo "No modpack detected. Add your modpack to contents/ModPacks/ and restart."
+  echo "No modpack detected. Add your modpack to ModPacks/ and restart. Don't forget to edit your serverconfig.txt"
   exit
 fi
