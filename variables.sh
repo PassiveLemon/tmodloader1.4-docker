49e25d2ef1207e9abc72891384c251a3ba9ca751
PassiveLemon <lemonl3mn@protonmail.com>
2023-07-12 13:15:38 -0400

    refactor: separate server variables for style

---
+++ variables.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Define server variables for config pasting later. Very crude and ugly
+AUTOCREATEx="autocreate=${AUTOCREATE}"
+DIFFICULTYx="difficulty=${DIFFICULTY}"
+BANLISTx="banlist=${BANLIST}"
+LANGUAGEx="language=${LANGUAGE}"
+MAXPLAYERSx="maxplayers=${MAXPLAYERS}"
+if [ "${MODPACK}" = "" ]; then
+  echo "|| Modpack name was not provided. Exiting... ||"
+  exit
+fi
+if [ "${MOTD}" != "" ]; then
+  MOTDx="motd=${MOTD}"
+fi
+NPCSTREAMx="npcstream=${NPCSTREAM}"
+if [ "${PASSWORD}" != "" ]; then
+  PASSWORDx="password=${PASSWORD}"
+fi
+if [ "${PORT}" = "" ]; then
+  echo "|| Port not set. Exiting... ||"
+  exit
+fi
+PORTx="port=${PORT}"
+PRIORITYx="priority=${PRIORITY}"
+SECUREx="secure=${SECURE}"
+if [ "${SEED}" != "" ]; then
+  SEEDx="seed=${SEED}"
+fi
+UPNPx="upnp=${UPNP}"
+WORLDNAMEx="worldname=${WORLDNAME}"
+
+# Automatically set variables
+WORLDx="world=/tmodloader/config/Worlds/${WORLDNAME}.wld"
+MODPACKx="modpack=/tmodloader/config/ModPacks/${MODPACK}/Mods/enabled.json"
+MODPATHx="modpath=/tmodloader/config/ModPacks/${MODPACK}/Mods/"
+
+if [ ! -e "/tmodloader/config/ModPacks/${MODPACK}/Mods/enabled.json" ]; then
+  echo "|| Modpack was not detected. Exiting... ||"
+  exit
+fi
+
+# Write variables to file
+cd /tmodloader/config/
+if [ ${SERVERCONFIG} = "0" ]; then
+  if [ -e /tmodloader/config/serverconfig.txt ]; then
+    rm /tmodloader/config/serverconfig.txt
+  fi
+  for argument in $AUTOCREATEx $DIFFICULTYx $BANLISTx $LANGUAGEx $MAXPLAYERSx $MOTDx $NPCSTREAMx $PASSWORDx $PORTx $PRIORITYx $SEEDx $SECUREx $UPNPx $WORLDNAMEx $WORLDx $MODPACKx $MODPATHx; do
+    echo $argument >> serverconfig.txt
+  done
+fi
