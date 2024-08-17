276c2e527d441d7021719a19e9e953f125b976b9
PassiveLemon <lemonl3mn@protonmail.com>
2024-08-17 14:33:26 -0400

    Fix: MOTD word splitting

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,48 +1,50 @@
 #!/usr/bin/env bash
 
+declare -A VAR_ARRAY
+
 # Define server variables for config pasting later. Very crude and ugly
-AUTOCREATEx="autocreate=${AUTOCREATE}"
-DIFFICULTYx="difficulty=${DIFFICULTY}"
-BANLISTx="banlist=${BANLIST}"
-LANGUAGEx="language=${LANGUAGE}"
-MAXPLAYERSx="maxplayers=${MAXPLAYERS}"
+VAR_ARRAY[AUTOCREATEx]="autocreate=${AUTOCREATE}"
+VAR_ARRAY[DIFFICULTYx]="difficulty=${DIFFICULTY}"
+VAR_ARRAY[BANLISTx]="banlist=${BANLIST}"
+VAR_ARRAY[LANGUAGEx]="language=${LANGUAGE}"
+VAR_ARRAY[MAXPLAYERSx]="maxplayers=${MAXPLAYERS}"
 if [ "$MODPACK" = "" ]; then
   echo "|| Modpack name was not provided. Exiting... ||"
   exit
 fi
 if [ "$MOTD" != "" ]; then
-  MOTDx="motd=${MOTD}"
+  VAR_ARRAY[MOTDx]="motd=${MOTD}"
 fi
-NPCSTREAMx="npcstream=${NPCSTREAM}"
+VAR_ARRAY[NPCSTREAMx]="npcstream=${NPCSTREAM}"
 if [ "$PASSWORD" != "" ]; then
-  PASSWORDx="password=${PASSWORD}"
+  VAR_ARRAY[PASSWORDx]="password=${PASSWORD}"
 fi
-PORTx="port=7777"
-PRIORITYx="priority=${PRIORITY}"
-SECUREx="secure=${SECURE}"
+VAR_ARRAY[PORTx]="port=7777"
+VAR_ARRAY[PRIORITYx]="priority=${PRIORITY}"
+VAR_ARRAY[SECUREx]="secure=${SECURE}"
 if [ "$SEED" != "" ]; then
-  SEEDx="seed=${SEED}"
+  VAR_ARRAY[SEEDx]="seed=${SEED}"
 fi
-UPNPx="upnp=${UPNP}"
-WORLDNAMEx="worldname=${WORLDNAME}"
+VAR_ARRAY[UPNPx]="upnp=${UPNP}"
+VAR_ARRAY[WORLDNAMEx]="worldname=${WORLDNAME}"
 
 # Automatically set variables
-WORLDx="world=/opt/tmodloader/config/Worlds/${WORLDNAME}.wld"
-MODPACKx="modpack=/opt/tmodloader/config/ModPacks/${MODPACK}/Mods/enabled.json"
-MODPATHx="modpath=/opt/tmodloader/config/ModPacks/${MODPACK}/Mods/"
+VAR_ARRAY[WORLDx]="world=/opt/terraria/config/Worlds/${WORLDNAME}.wld"
+VAR_ARRAY[MODPACKx]="modpack=/opt/tmodloader/config/ModPacks/${MODPACK}/Mods/enabled.json"
+VAR_ARRAY[MODPATHx]="modpath=/opt/tmodloader/config/ModPacks/${MODPACK}/Mods/"
+
+# Write variables to file
+if [ "$SERVERCONFIG" = "0" ]; then
+  if [ -e "/opt/terraria/config/serverconfig.txt" ]; then
+    rm /opt/terraria/config/serverconfig.txt
+  fi
+  for argument in "${VAR_ARRAY[@]}"; do
+    echo "$argument" >> /opt/terraria/config/serverconfig.txt
+  done
+fi
 
 if [ ! -e "/opt/tmodloader/config/ModPacks/${MODPACK}/Mods/enabled.json" ]; then
   echo "|| Modpack was not detected. Exiting... ||"
   exit
 fi
 
-# Write variables to file
-cd /opt/tmodloader/config/
-if [ "$SERVERCONFIG" = "0" ]; then
-  if [ -e "/opt/tmodloader/config/serverconfig.txt" ]; then
-    rm /opt/tmodloader/config/serverconfig.txt
-  fi
-  for argument in $AUTOCREATEx $DIFFICULTYx $BANLISTx $LANGUAGEx $MAXPLAYERSx $MOTDx $NPCSTREAMx $PASSWORDx $PORTx $PRIORITYx $SEEDx $SECUREx $UPNPx $WORLDNAMEx $WORLDx $MODPACKx $MODPATHx; do
-    echo $argument >> serverconfig.txt
-  done
-fi
